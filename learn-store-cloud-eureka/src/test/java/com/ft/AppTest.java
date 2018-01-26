aaba8850caccc5ad0c83ee5b41620b4568f7d88f
Xiao-Y <lyongtao123@126.com>
2018-01-26 18:36:27 +0800

    添加测试信息

--- learn-store-cloud-eureka/src/test/java/com/ft/AppTest.java
+++ learn-store-cloud-eureka/src/test/java/com/ft/AppTest.java
@@ -5,7 +5,7 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Unit test for simple App.
+ * Unit test for simple EurekaApp.
  */
 public class AppTest 
     extends TestCase
