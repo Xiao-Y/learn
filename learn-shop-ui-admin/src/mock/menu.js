f99e45805689e80d41ac639be8787462ae828a49
Xiao-Y <lyongtao123@126.com>
2018-05-04 19:24:46 +0800

    添加路由

---
+++ learn-shop-ui-admin/src/mock/menu.js
@@ -0,0 +1,28 @@
+var data = [
+  {
+    path: '/home',
+    name: '首页'
+  },
+  {
+    name: '系统组件',
+    child: [
+      {
+        name: '介绍',
+        path: '/components'
+      },
+      {
+        name: '功能类',
+        child: [
+          {
+            path: '/components/permission',
+            name: '详细鉴权'
+          },
+          {
+            path: '/components/pageTable',
+            name: '表格分页'
+          }
+        ]
+      }
+    ]
+  }
+]
--- learn-shop-ui-admin/src/router/index.js
+++ learn-shop-ui-admin/src/router/index.js
@@ -1,15 +1,35 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
-Vue.use(Router)
+Vue.use(Router);
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+const router = new VueRouter({
+  mode: 'history',
+  routes: staticRoute
+});
+
+// router为vue-router路由对象
+router.beforeEach((to, from, next) => {
+  // ajax获取权限列表函数
+  // 这里省略了一些判断条件，比如判断是否已经拥有了权限数据等
+  getPermission().then(res => {
+    let isPermission = false
+
+    permissionList.forEach((v) => {
+      // 判断跳转的页面是否在权限列表中
+      if (v.path == to.fullPath) {
+        isPermission = true
+      }
+    });
+
+    // 没有权限时跳转到401页面
+    if (!isPermission) {
+      next({path: "/error/401", replace: true})
+    } else {
+      next()
     }
-  ]
-})
+  });
+
+});
+
+export default router
