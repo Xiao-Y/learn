ec7afea96dce729b16c9f91f5b0f3d3e4c6211db
Xiao-Y <lyongtao123@126.com>
2018-05-14 16:08:17 +0800

    添加必要方法

--- learn-cloud-zuul/src/main/java/com/ft/filter/MyFilter.java
+++ learn-cloud-zuul/src/main/java/com/ft/filter/MyFilter.java
@@ -17,7 +17,31 @@ import javax.servlet.http.HttpServletRequest;
 @Component
 public class MyFilter extends ZuulFilter {
 
-    private static Logger log = LoggerFactory.getLogger(MyFilter.class);
+    private static Logger LOG = LoggerFactory.getLogger(MyFilter.class);
+
+    protected String getRemoteHost(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        LOG.debug("当前访问IP为：" + ip);
+        return ip;
+    }
+
+    protected String getClientType(HttpServletRequest request) {
+        String clientType = request.getHeader("User-Agent");
+        if (clientType == null || clientType.length() == 0 || "unknown".equalsIgnoreCase(clientType)) {
+            clientType = "";
+        }
+        LOG.debug("当前访问客户端类型为：" + clientType);
+        return clientType;
+    }
 
     /**
      * filterType：返回一个字符串代表过滤器的类型，在zuul中定义了四种不同生命周期的过滤器类型，具体如下：<br/>
@@ -62,7 +86,9 @@ public class MyFilter extends ZuulFilter {
     public Object run() {
         RequestContext ctx = RequestContext.getCurrentContext();
         HttpServletRequest request = ctx.getRequest();
-        log.info(String.format("%s >>> %s", request.getMethod(), request.getRequestURL().toString()));
+        this.getRemoteHost(request);
+        this.getClientType(request);
+        LOG.info(String.format("%s >>> %s", request.getMethod(), request.getRequestURL().toString()));
         return null;
     }
 }
