8452e03624f6bb647af57dc2b400a3e03c2700c3
Xiao-Y <lyongtao123@126.com>
2018-12-11 22:22:11 +0800

    完善认证授权中心

--- learn-shop-public-auth/src/main/java/com/billow/auth/api/UserApi.java
+++ learn-shop-public-auth/src/main/java/com/billow/auth/api/UserApi.java
@@ -1,11 +1,13 @@
 package com.billow.auth.api;
 
 import cn.hutool.json.JSONUtil;
+import com.billow.auth.properties.OAuth2Properties;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,20 +27,26 @@ public class UserApi {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UserApi.class);
 
-    @GetMapping("/user")
-//    public Object getCurrentUser1(Authentication authentication, HttpServletRequest request) throws UnsupportedEncodingException {
-//        LOGGER.info("【SecurityOauth2Application】 getCurrentUser1 authenticaiton={}", JSONUtil.toJsonStr(authentication));
-//        String header = request.getHeader("Authorization");
-//        String token = StringUtils.substringAfter(header, "bearer ");
-//
-//        Claims claims = Jwts.parser().setSigningKey("123".getBytes("UTF-8")).parseClaimsJws(token).getBody();
-//        String blog = (String) claims.get("blog");
-//        LOGGER.info("【SecurityOauth2Application】 getCurrentUser1 blog={}", blog);
-//
-//        return authentication;
-//    }
+    @Autowired
+    private OAuth2Properties oAuth2Properties;
 
-    public Principal user(Principal user) {
-        return user;
+    @GetMapping("/user")
+    public Object getCurrentUser1(Authentication authentication, HttpServletRequest request) throws UnsupportedEncodingException {
+        LOGGER.info("【SecurityOauth2Application】 getCurrentUser1 authenticaiton={}", JSONUtil.toJsonStr(authentication));
+        String header = request.getHeader("Authorization");
+        String token = StringUtils.substringAfter(header, "bearer ");
+        String access_token = request.getParameter("access_token");
+
+        Claims claims = Jwts.parser()
+                .setSigningKey(oAuth2Properties.getJwtSigningKey().getBytes("UTF-8"))
+                .parseClaimsJws(access_token).getBody();
+        String blog = claims.get("blog", String.class);
+        LOGGER.info("【SecurityOauth2Application】 getCurrentUser1 blog={}", blog);
+
+        return authentication;
     }
+
+//    public Principal user(Principal user) {
+//        return user;
+//    }
 }
