c08ac07f92f3e259c8765648191389be372f7f4c
Xiao-Y <lyongtao123@126.com>
2019-08-05 16:56:14 +0800

    添加：按钮组（禁用，删除，修改）组件

---
+++ learn-shop-ui-admin/src/components/common/CustomButtonGroup.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <el-tooltip class="item" effect="dark" content="禁用" placement="top-start" :open-delay="openDelay" v-if="showInd">
+      <el-button @click="onInd" type="warning" size="mini" :disabled="disInd">
+        <i class="el-icon-warning"></i>
+      </el-button>
+    </el-tooltip>
+    <el-tooltip class="item" effect="dark" content="删除" placement="top-start" :open-delay="openDelay" v-if="showDel">
+      <el-button @click="onDel" type="danger" size="mini">
+        <i class="el-icon-delete"></i>
+      </el-button>
+    </el-tooltip>
+    <el-tooltip class="item" effect="dark" content="修改" placement="top-start" :open-delay="openDelay" v-if="showEdit">
+      <el-button @click="onEdit" type="primary" size="mini">
+        <i class="el-icon-edit"></i></el-button>
+    </el-tooltip>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      // 提示信息延时
+      openDelay: {
+        type: Number,
+        default: 1500
+      },
+      // 是否显示禁用按钮
+      showInd: {
+        type: Boolean,
+        default: true
+      },
+      // 是否禁用禁用按钮
+      disInd:{
+        type: Boolean,
+        default: false
+      },
+      // 是否显示删除按钮
+      showDel: {
+        type: Boolean,
+        default: true
+      },
+      // 是否显示修改按钮
+      showEdit: {
+        type: Boolean,
+        default: true
+      }
+    },
+    data() {
+      return {}
+    },
+    methods: {
+      // 禁用事件
+      onInd() {
+        this.$emit('onInd');
+      },
+      // 删除事件
+      onDel() {
+        this.$emit('onDel');
+      },
+      // 修改事件
+      onEdit() {
+        this.$emit('onEdit');
+      },
+    }
+  }
+</script>
+
+<style>
+</style>
--- learn-shop-ui-admin/src/views/sys/WhiteList.vue
+++ learn-shop-ui-admin/src/views/sys/WhiteList.vue
@@ -38,48 +38,26 @@
     </el-row>
     <el-row>
       <template>
-        <el-table
-          :data="tableData"
-          border
-          style="width: 100%">
-          <el-table-column
-            fixed
-            prop="date"
-            label="日期"
-            width="150">
+        <el-table :data="tableData" border style="width: 100%">
+          <el-table-column fixed prop="date" label="日期" width="150">
           </el-table-column>
-          <el-table-column
-            prop="name"
-            label="姓名"
-            width="120">
+          <el-table-column prop="name" label="姓名" width="120">
           </el-table-column>
-          <el-table-column
-            prop="province"
-            label="省份"
-            width="120">
+          <el-table-column prop="province" label="省份" width="120">
           </el-table-column>
-          <el-table-column
-            prop="city"
-            label="市区"
-            width="120">
+          <el-table-column prop="city" label="市区" width="120">
           </el-table-column>
-          <el-table-column
-            prop="address"
-            label="地址"
-            width="300">
+          <el-table-column prop="address" label="地址">
           </el-table-column>
-          <el-table-column
-            prop="zip"
-            label="邮编"
-            width="120">
+          <el-table-column prop="zip" label="邮编" width="120">
           </el-table-column>
-          <el-table-column
-            fixed="right"
-            label="操作"
-            width="100">
+          <el-table-column fixed="right" label="操作" width="200">
             <template slot-scope="scope">
-              <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
-              <el-button type="text" size="small">编辑</el-button>
+              <!-- <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
+              <el-button type="text" size="small">编辑</el-button> -->
+              <custom-button-group @onDel="onDel(scope.row,scope.$index)" @onEdit="onEdit(scope.row,scope.$index)"
+                :disInd="!scope.row.valid"
+                @onInd="onInd(scope.row,scope.$index)"></custom-button-group>
             </template>
           </el-table-column>
         </el-table>
@@ -88,14 +66,8 @@
     <el-row>
       <template>
         <div class="block">
-          <el-pagination
-            background
-            @size-change="handleSizeChange"
-            @current-change="handleCurrentChange"
-            :current-page.sync="currentPage1"
-            :page-size="100"
-            layout="total, prev, pager, next"
-            :total="1000">
+          <el-pagination background @size-change="handleSizeChange" @current-change="handleCurrentChange"
+            :current-page.sync="currentPage1" :page-size="100" layout="total, prev, pager, next" :total="1000">
           </el-pagination>
         </div>
       </template>
@@ -104,9 +76,12 @@
 </template>
 
 <script>
-  import {Message} from "element-ui";
+  import CustomButtonGroup from '../../components/common/CustomButtonGroup.vue';
 
   export default {
+    components: {
+      CustomButtonGroup
+    },
 
     data() {
       return {
@@ -118,6 +93,7 @@
           province: '上海',
           city: '普陀区',
           address: '上海市普陀区金沙江路 1518 弄',
+          valid:false,
           zip: 200333
         }, {
           date: '2016-05-02',
@@ -125,6 +101,7 @@
           province: '上海',
           city: '普陀区',
           address: '上海市普陀区金沙江路 1518 弄',
+          valid:true,
           zip: 200333
         }, {
           date: '2016-05-04',
@@ -132,12 +109,14 @@
           province: '上海',
           city: '普陀区',
           address: '上海市普陀区金沙江路 1518 弄',
+          valid:true,
           zip: 200333
         }, {
           date: '2016-05-01',
           name: '王小虎',
           province: '上海',
           city: '普陀区',
+          valid:true,
           address: '上海市普陀区金沙江路 1518 弄',
           zip: 200333
         }]
@@ -155,6 +134,22 @@
       },
       handleCurrentChange(val) {
         console.log(`当前页: ${val}`);
+      },
+      onInd(row, index) {
+        row.valid = false;
+        console.info("onInd");
+        console.info("row.address", row.address);
+        console.info("index", index);
+      },
+      onDel(row, index) {
+        console.info("onDel");
+        console.info("row.address", row.address);
+        console.info("index", index);
+      },
+      onEdit(row, index) {
+        console.info("onEdit");
+        console.info("row.address", row.address);
+        console.info("index", index);
       }
     }
   }
@@ -171,21 +166,27 @@
 
   /*定义滚动条高宽及背景 高宽分别对应横竖滚动条的尺寸*/
   ::-webkit-scrollbar {
-    width: 3px; /*滚动条宽度*/
-    height: 3px; /*滚动条高度*/
+    width: 3px;
+    /*滚动条宽度*/
+    height: 3px;
+    /*滚动条高度*/
   }
 
   /*定义滚动条轨道 内阴影+圆角*/
   ::-webkit-scrollbar-track {
     -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
-    border-radius: 10px; /*滚动条的背景区域的圆角*/
-    background-color: white; /*滚动条的背景颜色*/
+    border-radius: 10px;
+    /*滚动条的背景区域的圆角*/
+    background-color: white;
+    /*滚动条的背景颜色*/
   }
 
   /*定义滑块 内阴影+圆角*/
   ::-webkit-scrollbar-thumb {
-    border-radius: 10px; /*滚动条的圆角*/
+    border-radius: 10px;
+    /*滚动条的圆角*/
     -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
-    background-color: #2e363f; /*滚动条的背景颜色*/
+    background-color: #2e363f;
+    /*滚动条的背景颜色*/
   }
 </style>
