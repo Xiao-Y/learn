29cf95dbba62b9928a7a501b8c5f7f3b7f9fb814
Xiao-Y <lyongtao123@126.com>
2019-08-09 17:43:13 +0800

    移除多余代码

--- learn-shop-public-auth/src/main/java/com/billow/auth/AuthServerApplication.java
+++ learn-shop-public-auth/src/main/java/com/billow/auth/AuthServerApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableHystrix
 @EnableFeignClients
 @EnableEurekaClient
 @SpringBootApplication
+@ComponentScan("com.billow")
 public class AuthServerApplication {
 
     public static void main(String[] args) {
