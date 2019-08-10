1aa529af0ff6b3965e5ef043668af32400bec7a5
Xiao-Y <lyongtao123@126.com>
2019-08-10 15:15:49 +0800

    优化：初始化方法

--- learn-shop-admin-system/src/main/java/com/billow/system/api/CacheApi.java
+++ learn-shop-admin-system/src/main/java/com/billow/system/api/CacheApi.java
@@ -1,6 +1,6 @@
 package com.billow.system.api;
 
-import com.billow.system.init.IStartLoading;
+import com.billow.system.init.StartLoading;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
@@ -9,12 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 
 /**
  * 缓存管理
@@ -29,47 +23,17 @@ import java.util.concurrent.TimeUnit;
 public class CacheApi {
 
     @Autowired
-    private Map<String, IStartLoading> startLoading;
-
-    // 线程池长期维持的线程数
-    private static final int corePoolSize = 8;
-    // 线程数的上限
-    private static final int maximumPoolSize = 10;
-    // 超过corePoolSize的线程的idle时长，超过这个时间，多余的线程会被回收。
-    private static final int keepAlivelime = 0;
-    // 任务的排队队列
-    private static final int capacity = 512;
+    private StartLoading startLoading;
 
     @PutMapping("/initAll")
     @ApiOperation("初始化所有缓存，initDictionary,initRoleMenu,initRolePermission")
     public boolean initAll() {
-        ExecutorService pool = new ThreadPoolExecutor(corePoolSize, maximumPoolSize,
-                keepAlivelime, TimeUnit.SECONDS,
-                new ArrayBlockingQueue<>(capacity), // 使用有界队列，避免OOM
-                new ThreadPoolExecutor.DiscardPolicy());
-        for (Map.Entry<String, IStartLoading> entry : startLoading.entrySet()) {
-            pool.execute(() -> entry.getValue().init());
-        }
-        pool.shutdown();
-        return true;
+        return startLoading.init(null);
     }
-
 
     @PutMapping("/init/{cacheType}")
     @ApiOperation("初始化指定的缓存，initDictionary,initRoleMenu,initRolePermission")
     public boolean initCacheByType(@PathVariable("cacheType") String cacheType) {
-        ExecutorService pool = new ThreadPoolExecutor(corePoolSize, maximumPoolSize,
-                keepAlivelime, TimeUnit.SECONDS,
-                new ArrayBlockingQueue<>(capacity), // 使用有界队列，避免OOM
-                new ThreadPoolExecutor.DiscardPolicy());
-        pool.execute(() -> {
-            IStartLoading initDictionary = startLoading.get(cacheType);
-            if (initDictionary != null) {
-                initDictionary.init();
-            }
-        });
-        pool.shutdown();
-        return true;
+        return startLoading.init(cacheType);
     }
-
 }
--- learn-shop-admin-system/src/main/java/com/billow/system/init/StartLoading.java
+++ learn-shop-admin-system/src/main/java/com/billow/system/init/StartLoading.java
@@ -1,14 +1,14 @@
 package com.billow.system.init;
 
-import com.billow.system.pojo.po.PermissionPo;
-import com.billow.system.pojo.po.RolePo;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 启动加载数据
@@ -22,10 +22,38 @@ public class StartLoading implements InitializingBean {
     @Autowired
     private Map<String, IStartLoading> startLoading;
 
+    // 线程池长期维持的线程数
+    private static final int corePoolSize = 8;
+    // 线程数的上限
+    private static final int maximumPoolSize = 10;
+    // 超过corePoolSize的线程的idle时长，超过这个时间，多余的线程会被回收。
+    private static final int keepAlivelime = 0;
+    // 任务的排队队列
+    private static final int capacity = 512;
+
     @Override
     public void afterPropertiesSet() throws Exception {
-        for (Map.Entry<String, IStartLoading> entry : startLoading.entrySet()) {
-            entry.getValue().init();
+        this.init(null);
+    }
+
+    public boolean init(String cacheType) {
+        ExecutorService pool = new ThreadPoolExecutor(corePoolSize, maximumPoolSize,
+                keepAlivelime, TimeUnit.SECONDS,
+                new ArrayBlockingQueue<>(capacity), // 使用有界队列，避免OOM
+                new ThreadPoolExecutor.DiscardPolicy());
+        if (cacheType == null) {
+            for (Map.Entry<String, IStartLoading> entry : startLoading.entrySet()) {
+                pool.execute(() -> entry.getValue().init());
+            }
+        } else {
+            pool.execute(() -> {
+                IStartLoading loading = startLoading.get(cacheType);
+                if (loading != null) {
+                    loading.init();
+                }
+            });
         }
+        pool.shutdown();
+        return true;
     }
 }
