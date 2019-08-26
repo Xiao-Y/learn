80cb7941187b6306628c19500a2f92bec92c838e
Xiao-Y <lyongtao123@126.com>
2019-08-26 19:38:25 +0800

    添加测试

--- learn-shop-admin-system/src/main/test/ActivitiExecuteTests.java
+++ learn-shop-admin-system/src/main/test/ActivitiExecuteTests.java
@@ -1,4 +1,6 @@
 import com.billow.base.workflow.component.WorkFlowExecute;
+import com.billow.base.workflow.diagram.ActUtils;
+import com.billow.base.workflow.vo.ProcessInstanceVo;
 import com.billow.system.AdminSystemApp;
 import org.activiti.engine.repository.Deployment;
 import org.junit.Test;
@@ -6,6 +8,10 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 
 /**
  * 工作流执行测试
@@ -27,5 +33,32 @@ public class ActivitiExecuteTests {
         Deployment leave = workFlowExecute.deploy("leave");
         System.out.println("id:" + leave.getId());
         System.out.println("key:" + leave.getKey());
+    }
+
+    @Test
+    public void startProcessInstance() {
+        ProcessInstanceVo processInstanceVo = workFlowExecute.startProcessInstance("key", "test3", "0000", null);
+        System.out.println(processInstanceVo.getProcessInstanceId());
+    }
+
+    @Test
+    public void commitProcess() {
+        workFlowExecute.commitProcess("37514", null);
+    }
+
+
+    @Autowired
+    private ActUtils actUtils;
+
+    @Test
+    public void flowImg() throws Exception {
+        InputStream inputStream = actUtils.getFlowImgByInstanceId("32505");
+        FileOutputStream outputStream = new FileOutputStream(new File("D:\\aa.png"));
+        // 输出图片内容
+        byte[] b = new byte[1024];
+        int len;
+        while ((len = inputStream.read(b, 0, 1024)) != -1) {
+            outputStream.write(b, 0, len);
+        }
     }
 }
