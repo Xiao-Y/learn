b04a110a9bc1a3fbc0d9cd4dade32988e6062d1b
Xiao-Y <lyongtao123@126.com>
2019-08-27 12:04:23 +0800

    优化：工作流图片高亮

--- learn-shop-base-workflow/src/main/java/com/billow/base/workflow/diagram/ActUtils.java
+++ learn-shop-base-workflow/src/main/java/com/billow/base/workflow/diagram/ActUtils.java
@@ -4,22 +4,18 @@ import org.activiti.bpmn.model.BpmnModel;
 import org.activiti.bpmn.model.FlowNode;
 import org.activiti.bpmn.model.SequenceFlow;
 import org.activiti.engine.HistoryService;
-import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
 import org.activiti.engine.history.HistoricActivityInstance;
 import org.activiti.engine.history.HistoricProcessInstance;
-import org.activiti.image.ProcessDiagramGenerator;
-import org.apache.commons.lang3.StringUtils;
+import org.activiti.engine.repository.ProcessDefinition;
+import org.activiti.engine.runtime.Execution;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -27,86 +23,78 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * 流程工具类
+ * 流程图生成工具
  *
- * @author linyb1
+ * @author liuyongtao
+ * @create 2019-08-27 8:31
  */
 @Component
 public class ActUtils {
 
     private static Logger logger = LoggerFactory.getLogger(ActUtils.class);
 
-//	private static RepositoryService repositoryService			= SpringContextUtils.getBean(RepositoryService.class);
-//	private static HistoryService historyService				= SpringContextUtils.getBean(HistoryService.class);
-//	private static RuntimeService runtimeService				= SpringContextUtils.getBean(RuntimeService.class);
-//	private static TaskService taskService					= SpringContextUtils.getBean(TaskService.class);
-//	private static ProcessEngineConfiguration processEngineConfiguration	= SpringContextUtils.getBean(ProcessEngineConfiguration.class);
-
-    @Autowired
-    private ProcessEngine processEngine;
     @Autowired
     private HistoryService historyService;
     @Autowired
     private RepositoryService repositoryService;
+    @Autowired
+    private RuntimeService runtimeService;
 
     /**
-     * 根据流程实例Id,获取实时流程图片
+     * 获取流程图，并且显示活动节点.
      *
-     * @param processInstanceId
-     * @return
+     * @param executionId Execution对象ID，任务ID或流程实例ID等正在执行的对象ID
+     * @return 流程图输入流
      */
-    public InputStream getFlowImgByInstanceId(String processInstanceId) {
+    public InputStream generateProcessImage(String executionId) {
         InputStream imageStream = null;
-        try {
-            if (StringUtils.isEmpty(processInstanceId)) {
-                logger.error("processInstanceId is null");
-                return imageStream;
-            }
-            // 获取历史流程实例
-            HistoricProcessInstance historicProcessInstance = historyService.createHistoricProcessInstanceQuery().processInstanceId(processInstanceId).singleResult();
-            // 获取流程中已经执行的节点，按照执行先后顺序排序
-            List<HistoricActivityInstance> historicActivityInstances = historyService.createHistoricActivityInstanceQuery().processInstanceId(processInstanceId)
-                    .orderByHistoricActivityInstanceId().asc().list();
-            // 高亮已经执行流程节点ID集合
-//            List<String> highLightedActivitiIds = new ArrayList<>();
-//            for (HistoricActivityInstance historicActivityInstance : historicActivityInstances) {
-//                highLightedActivitiIds.add(historicActivityInstance.getActivityId());
-//            }
-
-            // 高亮已经执行流程节点ID集合
-            List<String> highLightedActivitiIds = new ArrayList<>();
-            for (HistoricActivityInstance historicActivityInstance : historicActivityInstances) {
-                if (historicActivityInstance.getEndTime() != null) {
-                    highLightedActivitiIds.add(historicActivityInstance.getActivityId());
-                }
-            }
-
-            List<HistoricProcessInstance> historicFinishedProcessInstances = historyService.createHistoricProcessInstanceQuery().processInstanceId(processInstanceId).finished()
-                    .list();
-            ProcessDiagramGenerator processDiagramGenerator = null;
-            // 如果还没完成，流程图高亮颜色为绿色，如果已经完成为红色
-            if (!CollectionUtils.isEmpty(historicFinishedProcessInstances)) {
-                // 如果不为空，说明已经完成
-                processDiagramGenerator = processEngine.getProcessEngineConfiguration().getProcessDiagramGenerator();
-            } else {
-                processDiagramGenerator = new CustomProcessDiagramGenerator();
-            }
-
-            BpmnModel bpmnModel = repositoryService.getBpmnModel(historicProcessInstance.getProcessDefinitionId());
-            // 高亮流程已发生流转的线id集合
-            List<String> highLightedFlowIds = getHighLightedFlows(bpmnModel, historicActivityInstances);
-
-            // 使用默认配置获得流程图表生成器，并生成追踪图片字符流
-            imageStream = processDiagramGenerator.generateDiagram(bpmnModel, "png", highLightedActivitiIds, highLightedFlowIds, "宋体", "微软雅黑", "黑体", null, 2.0);
+        if (executionId == null || "".equals(executionId)) {
+            logger.error("executionId is null");
             return imageStream;
+        }
+        try {
+            // 查询Execution对象
+            Execution execution = runtimeService.createExecutionQuery().executionId(executionId).singleResult();
+            String processInstanceId = execution.getProcessInstanceId();
+            // 查询历史流程实例
+            HistoricProcessInstance historicProcessInstance = historyService.createHistoricProcessInstanceQuery()
+                    .processInstanceId(processInstanceId)
+                    .singleResult();
+            // 查询流程定义对象
+            ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery()
+                    .processDefinitionId(historicProcessInstance.getProcessDefinitionId())
+                    .singleResult();
+            // 查询已经执行过的流程节点，包括Gateway等
+            List<HistoricActivityInstance> highLightedActivitList = historyService.createHistoricActivityInstanceQuery()
+                    .processInstanceId(processInstanceId)
+                    .list();
+            // 高亮线路id集合
+            BpmnModel bpmnModel = repositoryService.getBpmnModel(processDefinition.getId());
+            List<String> highLightedFlows = getHighLightedFlows(bpmnModel, highLightedActivitList);
+            logger.debug("Executed flow : {}", highLightedFlows);
+            // 高亮环节id集合
+            List<String> highLightedActivities = new ArrayList<>();
+            for (HistoricActivityInstance tempActivity : highLightedActivitList) {
+                String activityId = tempActivity.getActivityId();
+                highLightedActivities.add(activityId);
+            }
+            logger.debug("Executed activity: {}", highLightedActivities);
+            // 得到正在执行的Activity的Id
+            List<String> activityIds = new ArrayList<>();
+            getCurrrentActivity(processInstanceId, activityIds);
+            logger.debug("Current activity ids : {}", activityIds);
+            // 中文显示的是口口口，设置字体就好了
+            String font = "黑体";
+            imageStream = new CustomProcessDiagramGenerator().generateDiagram(bpmnModel, "png", highLightedActivities,
+                    highLightedFlows, font, font, font, activityIds);
         } catch (Exception e) {
-            logger.error("processInstanceId" + processInstanceId + "生成流程图失败，原因：" + e.getMessage(), e);
+            logger.error("executionId" + executionId + "生成流程图失败，原因：" + e.getMessage(), e);
         }
         return imageStream;
     }
 
     /**
-     * 获取已经流转的线
+     * 获取已经执行过的流程线，用于高亮显示
      *
      * @param bpmnModel
      * @param historicActivityInstances
@@ -128,23 +116,23 @@ public class ActUtils {
             }
         }
 
-        FlowNode currentFlowNode = null;
-        FlowNode targetFlowNode = null;
+        FlowNode currentFlowNode;
+        FlowNode targetFlowNode;
         // 遍历已完成的活动实例，从每个实例的outgoingFlows中找到已执行的
         for (HistoricActivityInstance currentActivityInstance : finishedActivityInstances) {
             // 获得当前活动对应的节点信息及outgoingFlows信息
             currentFlowNode = (FlowNode) bpmnModel.getMainProcess().getFlowElement(currentActivityInstance.getActivityId(), true);
             List<SequenceFlow> sequenceFlows = currentFlowNode.getOutgoingFlows();
-
             /**
-             * 遍历outgoingFlows并找到已已流转的 满足如下条件认为已已流转： 1.当前节点是并行网关或兼容网关，则通过outgoingFlows能够在历史活动中找到的全部节点均为已流转 2.当前节点是以上两种类型之外的，通过outgoingFlows查找到的时间最早的流转节点视为有效流转
+             * 遍历outgoingFlows并找到已已流转的 满足如下条件认为已已流转：
+             * 1.当前节点是并行网关或兼容网关，则通过outgoingFlows能够在历史活动中找到的全部节点均为已流转
+             * 2.当前节点是以上两种类型之外的，通过outgoingFlows查找到的时间最早的流转节点视为有效流转
              */
             if ("parallelGateway".equals(currentActivityInstance.getActivityType()) || "inclusiveGateway".equals(currentActivityInstance.getActivityType())) {
                 // 遍历历史活动节点，找到匹配流程目标节点的
                 for (SequenceFlow sequenceFlow : sequenceFlows) {
                     targetFlowNode = (FlowNode) bpmnModel.getMainProcess().getFlowElement(sequenceFlow.getTargetRef(), true);
                     if (historicActivityNodes.contains(targetFlowNode)) {
-//                        highLightedFlowIds.add(targetFlowNode.getId());
                         highLightedFlowIds.add(sequenceFlow.getId());
                     }
                 }
@@ -172,40 +160,28 @@ public class ActUtils {
                             earliestStamp = highLightedFlowStartTime;
                         }
                     }
-
                     highLightedFlowIds.add(highLightedFlowId);
                 }
-
             }
-
         }
         return highLightedFlowIds;
     }
 
-    public void saveBit(InputStream inStream, String filePath) throws IOException {
-
-        ByteArrayOutputStream outStream = new ByteArrayOutputStream();
-        //创建一个Buffer字符串
-        byte[] buffer = new byte[1024];
-        //每次读取的字符串长度，如果为-1，代表全部读取完毕
-        int len = 0;
-        //使用一个输入流从buffer里把数据读取出来
-        while ((len = inStream.read(buffer)) != -1) {
-            //用输出流往buffer里写入数据，中间参数代表从哪个位置开始读，len代表读取的长度
-            outStream.write(buffer, 0, len);
+    /**
+     * 获取流程当前的节点
+     *
+     * @param processInstanceId
+     * @param activityIds
+     * @return void
+     * @author LiuYongTao
+     * @date 2019/8/27 8:57
+     */
+    private void getCurrrentActivity(String processInstanceId, List<String> activityIds) {
+        // 查询流程当前活动的执行对象，代办任务节点
+        List<Execution> executions = runtimeService.createExecutionQuery().processInstanceId(processInstanceId).list();
+        for (Execution exe : executions) {
+            List<String> ids = runtimeService.getActiveActivityIds(exe.getId());
+            activityIds.addAll(ids);
         }
-        //关闭输入流
-        inStream.close();
-        //把outStream里的数据写入内存
-
-        //得到图片的二进制数据，以二进制封装得到数据，具有通用性
-        byte[] data = outStream.toByteArray();
-        //new一个文件对象用来保存图片，默认保存当前工程根目录
-        File imageFile = new File(filePath);
-        //创建输出流
-        FileOutputStream fileOutStream = new FileOutputStream(imageFile);
-        //写入数据
-        fileOutStream.write(data);
-
     }
 }
