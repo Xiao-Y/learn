7998c05dd8446ff3de17ad7a37756c15c54f04a1
Xiao-Y <lyongtao123@126.com>
2019-12-01 10:17:38 +0800

    修改：数据库驱动

---
+++ learn-shop-admin-user/src/main/java/com/billow/user/pojo/vo/TestVo.java
@@ -0,0 +1,42 @@
+//package com.billow.user.pojo.vo;
+//
+//import com.billow.user.pojo.po.TestPo;
+//
+//import java.io.Serializable;
+//import java.util.List;
+//
+///**
+// * @author liuyongtao
+// * @create 2018-04-26 9:42
+// */
+//public class TestVo extends TestPo implements Serializable {
+//    private CityVo cityVo;
+//
+//    private List<CityVo> cityVos;
+//
+//    public List<CityVo> getCityVos() {
+//        return cityVos;
+//    }
+//
+//    public TestVo setCityVos(List<CityVo> cityVos) {
+//        this.cityVos = cityVos;
+//        return this;
+//    }
+//
+//    public CityVo getCityVo() {
+//        return cityVo;
+//    }
+//
+//    public TestVo setCityVo(CityVo cityVo) {
+//        this.cityVo = cityVo;
+//        return this;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "TestVo{" +
+//                "cityVo=" + cityVo +
+//                ", cityVos=" + cityVos +
+//                "} " + super.toString();
+//    }
+//}
