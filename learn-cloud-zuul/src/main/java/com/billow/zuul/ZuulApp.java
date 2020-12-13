6c801e4bc1e24af2d818558ea5fcc02affc98e24
Xiao-Y <lyongtao123@126.com>
2020-12-13 16:07:17 +0800

    所有应用注册中心和配置中心都切换成 alibaba-nacos

--- learn-cloud-zuul/src/main/java/com/billow/zuul/ZuulApp.java
+++ learn-cloud-zuul/src/main/java/com/billow/zuul/ZuulApp.java
@@ -3,18 +3,14 @@ package com.billow.zuul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.ComponentScan;
 
 
-//@EnableHystrix
-//@EnableHystrixDashboard
-//@EnableFeignClients
 @EnableZuulProxy
-@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 @ComponentScan("com.billow")
 @EnableConfigurationProperties(SecurityProperties.class)
