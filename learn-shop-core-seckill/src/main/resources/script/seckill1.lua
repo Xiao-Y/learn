9b7a872e030809a1ce8ab4d0e36ce8413d8f6cd7
刘勇涛 <122356560@hdjt.hdad.local>
2021-08-11 15:22:45 +0800

    添加测试

--- learn-shop-core-seckill/src/main/resources/script/seckill1.lua
+++ learn-shop-core-seckill/src/main/resources/script/seckill1.lua
@@ -1,38 +1,38 @@
---- 库存key
-seckillStockKey = KEYS[1];
---- 秒杀key
-seckillLockKey = KEYS[2];
---- 获取秒杀商品信息
-successKillInfo = ARGV[1];
---- 获取秒杀商品超时时间
-expire = ARGV[2];
-
---- 获取指定的库存
-local resultStock = redis.call("get", seckillStockKey);
---- 是否库存存在
-if not resultStock then
-    return -6;
-end
-
---- 库存为空或者小于等0时返回
-if resultStock == nil or tonumber(resultStock) <= 0 then
-    return -4;
-end
-
---- 已经秒杀过
-if redis.call("get", seckillLockKey) then
-    return -1;
-end
-
---- 递减库存
-redis.call("decr", seckillStockKey);
---- 保存秒杀成功信息
-redis.call("set", seckillLockKey, successKillInfo);
---- 转换分钟为秒
-if tonumber(expire) > 0 then
-    local paymentExp = tonumber(expire) * 60;
-    --- 设置过期时间
-    redis.call("expire", seckillLockKey, paymentExp);
-end
-return 1;
-
+----- 库存key
+--local seckillStockKey = KEYS[1];
+----- 秒杀key
+--local seckillLockKey = KEYS[2];
+----- 获取秒杀商品信息
+--local successKillInfo = ARGV[1];
+----- 获取秒杀商品超时时间
+--local expire = ARGV[2];
+--
+----- 获取指定的库存
+--local resultStock = redis.call("get", seckillStockKey);
+----- 是否库存存在
+--if not resultStock then
+--    return -6;
+--end
+--
+----- 库存为空或者小于等0时返回
+--if resultStock == nil or tonumber(resultStock) <= 0 then
+--    return -4;
+--end
+--
+----- 已经秒杀过
+--if redis.call("get", seckillLockKey) then
+--    return -1;
+--end
+--
+----- 递减库存
+--redis.call("decr", seckillStockKey);
+----- 保存秒杀成功信息
+--redis.call("set", seckillLockKey, successKillInfo);
+----- 转换分钟为秒
+--if tonumber(expire) > 0 then
+--    local paymentExp = tonumber(expire) * 60;
+--    --- 设置过期时间
+--    redis.call("expire", seckillLockKey, paymentExp);
+--end
+--return 1;
+--
