da74f4eb800e874333693ab888419bc64b5c0fc9
Xiao-Y <lyongtao123@126.com>
2021-12-14 22:50:14 +0800

    修改配置文件

---
+++ learn-shop-base/learn-shop-base-notice/src/main/java/com/billow/notice/amqp/yml/MqCollectProperties.java
@@ -0,0 +1,47 @@
+package com.billow.notice.amqp.yml;
+
+import lombok.Data;
+import org.springframework.amqp.core.ExchangeTypes;
+
+/**
+ * mq 的队列、路由、交换机配置
+ *
+ * @author xiaoy
+ * @since 2021/12/14 22:40
+ */
+@Data
+public class MqCollectProperties {
+
+    /**
+     * 交换机名称
+     *
+     * @author xiaoy
+     * @since 2021/12/14 22:40
+     */
+    private String exchange;
+
+    /**
+     * 交换机类型，默认 direct
+     * {@link ExchangeTypes}
+     *
+     * @author xiaoy
+     * @since 2021/12/14 22:47
+     */
+    private String exchangeType = ExchangeTypes.DIRECT;
+
+    /**
+     * 队列
+     *
+     * @author xiaoy
+     * @since 2021/12/14 22:40
+     */
+    private String queue;
+
+    /**
+     * 路由
+     *
+     * @author xiaoy
+     * @since 2021/12/14 22:40
+     */
+    private String routeKey;
+}
---
+++ learn-shop-base/learn-shop-base-notice/src/main/java/com/billow/notice/amqp/yml/NoticeMqYml.java
@@ -0,0 +1,74 @@
+package com.billow.notice.amqp.yml;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * mq 消息通知配置文件
+ *
+ * @author xiaoy
+ * @since 2021/12/14 22:37
+ */
+@Data
+@ConfigurationProperties(prefix = "notice.mq")
+public class NoticeMqYml {
+
+    /**
+     * 中间件类型
+     *
+     * @author xiaoy
+     * @since 2021/12/14 22:42
+     */
+    private String type;
+
+    /**
+     * 服务器地址
+     *
+     * @author xiaoy
+     * @since 2021/12/14 22:42
+     */
+    private String host;
+
+    /**
+     * 服务器端口
+     *
+     * @author xiaoy
+     * @since 2021/12/14 22:42
+     */
+    private int port;
+
+    /**
+     * 用户名和密码
+     *
+     * @author xiaoy
+     * @since 2021/12/14 22:42
+     */
+    private String username;
+
+    /**
+     * 用户名和密码
+     *
+     * @author xiaoy
+     * @since 2021/12/14 22:42
+     */
+    private String password;
+
+    /**
+     * 虚拟主机
+     *
+     * @author xiaoy
+     * @since 2021/12/14 22:42
+     */
+    private String virtualHost;
+
+    /**
+     * mq 的队列、路由、交换机配置
+     *
+     * @author xiaoy
+     * @since 2021/12/14 22:42
+     */
+    private List<MqCollectProperties> mqCollect = new ArrayList<>();
+}
