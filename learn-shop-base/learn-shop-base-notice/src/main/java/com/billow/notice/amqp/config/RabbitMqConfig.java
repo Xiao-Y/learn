7076f40cbf111daa6d7cff18ee991cf4dce8e415
千面 <qianmian@janezt.com>
2021-12-18 13:38:23 +0800

    完成 mq 的整合3

--- learn-shop-base/learn-shop-base-notice/src/main/java/com/billow/notice/amqp/config/RabbitMqConfig.java
+++ learn-shop-base/learn-shop-base-notice/src/main/java/com/billow/notice/amqp/config/RabbitMqConfig.java
@@ -2,15 +2,17 @@ package com.billow.notice.amqp.config;
 
 import com.billow.notice.amqp.properties.NoticeMqYml;
 import com.billow.notice.amqp.service.SendMQService;
-import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListenerConfigurer;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistrar;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.handler.annotation.support.DefaultMessageHandlerMethodFactory;
+import org.springframework.messaging.handler.annotation.support.MessageHandlerMethodFactory;
 
 /**
  * rabbit mq 配置文件
@@ -20,9 +22,45 @@ import org.springframework.context.annotation.Scope;
  */
 @Configuration
 @EnableConfigurationProperties({NoticeMqYml.class})
-public class RabbitMqConfig {
+public class RabbitMqConfig implements RabbitListenerConfigurer
+{
+    @Override
+    public void configureRabbitListeners(RabbitListenerEndpointRegistrar registrar)
+    {
+        registrar.setMessageHandlerMethodFactory(messageHandlerMethodFactory());
+    }
+
     @Bean
-    public SendMQService sendMQService() {
+    public MessageHandlerMethodFactory messageHandlerMethodFactory()
+    {
+        DefaultMessageHandlerMethodFactory messageHandlerMethodFactory = new DefaultMessageHandlerMethodFactory();
+        messageHandlerMethodFactory.setMessageConverter(consumerJackson2MessageConverter());
+        return messageHandlerMethodFactory;
+    }
+
+    @Bean
+    public MappingJackson2MessageConverter consumerJackson2MessageConverter()
+    {
+        return new MappingJackson2MessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory)
+    {
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(producerJackson2MessageConverter());
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter producerJackson2MessageConverter()
+    {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public SendMQService sendMQService()
+    {
         return new SendMQService();
     }
 }
