991725943fb3af500d520b0e9a2b9e614272c476
Xiao-Y <lyongtao123@126.com>
2021-12-24 09:23:17 +0800

    测试推送

--- learn-shop-ui-admin/src/router/baseRouter.js
+++ learn-shop-ui-admin/src/router/baseRouter.js
@@ -1,26 +1,26 @@
-import Home from "../views/home/Home";
-
-/**
- * 固定路由
- */
-export const constantRouterMap = [{
-  path: '/',
-  redirect: '/login',
-  hidden: true
-}, {
-  path: '/login',
-  name: '登录页面',
-  hidden: true,
-  component: resolve => require(['../views/login/Login.vue'], resolve)
-}, {
-  path: '/error',
-  name: 'error',
-  component: Home,
-  children: [{
-    name: '404',
-    path: '404',
-    component: resolve => require(['../views/error/404.vue'], resolve)
-  }]
-}];
-
-export default constantRouterMap
+// import Home from "../views/home/Home";
+//
+// /**
+//  * 固定路由
+//  */
+// export const constantRouterMap = [{
+//   path: '/',
+//   redirect: '/login',
+//   hidden: true
+// }, {
+//   path: '/login',
+//   name: '登录页面',
+//   hidden: true,
+//   component: resolve => require(['../views/login/Login.vue'], resolve)
+// }, {
+//   path: '/error',
+//   name: 'error',
+//   component: Home,
+//   children: [{
+//     name: '404',
+//     path: '404',
+//     component: resolve => require(['../views/error/404.vue'], resolve)
+//   }]
+// }];
+//
+// export default constantRouterMap
