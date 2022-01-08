ff3d7147de4639dff852fc04f8183216dff4f323
Xiao-Y <lyongtao123@126.com>
2022-01-08 16:00:17 +0800

    修改：商品系统启动异常

--- learn-shop-core-product/src/main/java/com/billow/product/api/GoodsSpuSpecApi.java
+++ learn-shop-core-product/src/main/java/com/billow/product/api/GoodsSpuSpecApi.java
@@ -2,14 +2,20 @@ package com.billow.product.api;
 
 import com.billow.mybatis.base.HighLevelApi;
 import com.billow.product.pojo.build.GoodsSpuSpecBuildParam;
-import com.billow.product.pojo.search.GoodsSpuSpecSearchParam;
 import com.billow.product.pojo.po.GoodsSpuSpecPo;
+import com.billow.product.pojo.search.GoodsSpuSpecSearchParam;
 import com.billow.product.pojo.vo.GoodsSpuSpecVo;
 import com.billow.product.service.GoodsSpuSpecService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -26,4 +32,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/goodsSpuSpecApi")
 public class GoodsSpuSpecApi extends HighLevelApi<GoodsSpuSpecService, GoodsSpuSpecPo, GoodsSpuSpecVo, GoodsSpuSpecBuildParam, GoodsSpuSpecSearchParam> {
 
+    @Autowired
+    private GoodsSpuSpecService goodsSpuSpecService;
+
+    @ApiOperation(value = "根据 spuId 查询 spu 规格Key数据")
+    @GetMapping(value = "/findSpuSpecKey/{spuId}")
+    public List<Long> findSpuSpecKey(@PathVariable Long spuId) {
+        return goodsSpuSpecService.findSpuSpecKey(spuId);
+    }
 }
--- learn-shop-core-product/src/main/java/com/billow/product/service/GoodsSpuSpecService.java
+++ learn-shop-core-product/src/main/java/com/billow/product/service/GoodsSpuSpecService.java
@@ -5,6 +5,9 @@ import com.billow.mybatis.base.HighLevelService;
 import com.billow.product.pojo.po.GoodsSpuSpecPo;
 import com.billow.product.pojo.search.GoodsSpuSpecSearchParam;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * <p>
  * spu规格表 服务类
@@ -14,6 +17,27 @@ import com.billow.product.pojo.search.GoodsSpuSpecSearchParam;
  * @version v2.0
  * @since 2021-09-02
  */
-public interface GoodsSpuSpecService extends HighLevelService<GoodsSpuSpecPo,GoodsSpuSpecSearchParam> {
+public interface GoodsSpuSpecService extends HighLevelService<GoodsSpuSpecPo, GoodsSpuSpecSearchParam> {
+
+
+    /**
+     * 根据 spuId 查询spu规格表数据
+     *
+     * @param spuId
+     * @return java.util.List<java.util.Map < java.lang.String, java.lang.String>>
+     * @author LiuYongTao
+     * @date 2019/11/27 11:33
+     */
+    List<Map<String, Object>> findSpuSpec(Long spuId);
+
+    /**
+     * 根据 spuId 查询 spu 规格Key数据
+     *
+     * @param spuId
+     * @return java.util.List<java.lang.String>
+     * @author LiuYongTao
+     * @date 2019/12/5 16:33
+     */
+    List<Long> findSpuSpecKey(Long spuId);
 
 }
