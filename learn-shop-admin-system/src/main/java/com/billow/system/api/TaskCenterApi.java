554c835039367551b958297df6158625813de9b8
Xiao-Y <lyongtao123@126.com>
2026-03-01 10:18:46 +0800

    chore(api): 修改任务中心API路径映射

--- learn-shop-admin-system/src/main/java/com/billow/system/api/TaskCenterApi.java
+++ learn-shop-admin-system/src/main/java/com/billow/system/api/TaskCenterApi.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/task")
+@RequestMapping("/taskApi")
 public class TaskCenterApi {
     @Autowired
     private  TaskFlowService taskFlowService;
